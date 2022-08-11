9f4f6017fab49fb22f14c9a19d7468e8afd0aa5f
Mateus Costa dos Santos <mtscostadossantos@gmail.com>
2022-08-11 09:24:04 -0300

    Update Teste

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql");
+require('dotenv/config');
+
+
 
 const db = mysql.createConnection({
-    host: "database-sag.cxquo8blexhf.us-east-1.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "SantoGraal",
-    database: "sag"
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 
 module.exports = {
